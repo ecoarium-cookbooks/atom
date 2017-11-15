72df6f92f88782da42f5fe3083c15b8968570994
Jay Flowers <jay.flowers@gmail.com>
2017-11-15 12:34:06 -0500

    commit for version : 0.2.41

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mohit@sethis.in'
 license 'Apache v2.0'
 description 'Installs/Configures the Atom text editor'
 long_description 'Installs/Configures the Atom text editor - a hackable editor based on Electron, maintained by GitHub'
-version '0.2.40'
+version '0.2.41'
 
 recipe 'atom::default', 'Installs/Configures Atom'
 
@@ -13,8 +13,8 @@ supports 'windows'
 supports 'ubuntu'
 supports 'debian'
 
-depends 'apt', '~> 3.0'
-depends 'homebrew', '~> 2.1'
+depends 'apt'
+depends 'homebrew'
 
 source_url 'https://github.com/mohitsethi/chef-atom'
 issues_url 'https://github.com/mohitsethi/chef-atom/issues'
